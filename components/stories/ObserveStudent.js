391ca0a97b79ff48df383d48646b21eead4a3d21
Tyler Collier <git@tylercollier.com>
2017-05-08 10:29:10 -0700

    Use multiple stories to show off UI differences

--- components/stories/ObserveStudent.js
+++ components/stories/ObserveStudent.js
@@ -5,32 +5,48 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 storiesOf('ObserveStudent', module)
-  .add('basic', () => {
-    return getMainSection();
-  })
-
-function Users(prop) {
-  let items = [],
-      classMd = 3,
-      userLength = prop.children.length;
-    if(userLength == 2) classMd = 6;
-    for(var i=0; i <= userLength-1; i++) {
-      items.push(<Col md={classMd}> {prop.children[i]()}  </Col>);
-    }
-  return (
-    <Row>
-      {items}
-    </Row>
-  );
-}
-
-function getMainSection() {
-  return (
+  .add('2 students', () => {
+    return (
     <Grid>
       <Users>
         {(user) => <ObserveStudent toggle="toggle-task-1" />}
         {(user) => <ObserveStudent toggle="toggle-task-2" />}
       </Users>
     </Grid>
+  );
+  })
+  .add('3 students', () => {
+    return (
+    <Grid>
+      <Users>
+        {(user) => <ObserveStudent toggle="toggle-task-1" />}
+        {(user) => <ObserveStudent toggle="toggle-task-2" />}
+        {(user) => <ObserveStudent toggle="toggle-task-3" />}
+      </Users>
+    </Grid>
+  );
+  })
+  .add('4 students', () => {
+    return (
+    <Grid>
+      <Users>
+        {(user) => <ObserveStudent toggle="toggle-task-1" />}
+        {(user) => <ObserveStudent toggle="toggle-task-2" />}
+        {(user) => <ObserveStudent toggle="toggle-task-3" />}
+        {(user) => <ObserveStudent toggle="toggle-task-4" />}
+      </Users>
+    </Grid>
+  );
+  })
+
+function Users(prop) {
+  let classMd = 3;
+  const userLength = prop.children.length;
+  if(userLength == 2) classMd = 6;
+  const items = Array(userLength).fill().map((_,i) => <Col md={classMd}> {prop.children[i]()}  </Col>);
+  return (
+    <Row>
+      {items}
+    </Row>
   );
 }
